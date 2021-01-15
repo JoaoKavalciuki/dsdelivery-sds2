157de1d3218904b73f33c091c2472cb682ed8f94
João Kavalciuki <joaokavalciuki@hotmail.com>
2021-01-15 05:20:20 -0300

    Add front-mobile initial files

--- front-web/src/orders/index.tsx
+++ front-web/src/orders/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
+import Footer from "../footer";
 import { fetchProducts } from "./api";
+import { checkIsSelected } from "./helpers";
+
 import OrderLocation from "./OrderLocation";
+import OrderSummary from "./OrderSummary";
 import ProductList from "./ProductList";
 import StepsHeader from "./StepsHeader";
 import "./style.css";
@@ -8,8 +12,11 @@ import { OrderLocationData, Product } from "./types";
 
 function Orders()  {
     const[products, setProducts] = useState<Product[]>([]);
+    const[selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
-    
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0)
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
@@ -17,16 +24,39 @@ function Orders()  {
         
     } , [])
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return(
-        <div className = "orders-container">
-            <StepsHeader/>
-            <ProductList products = {products}/>
-            <OrderLocation 
-            onChangeLocation = {
-                location => setOrderLocation(location)
-                }
-            />
-        </div>
+        //I was doing the summary of itens.
+        <>
+            <div className = "orders-container">
+                <StepsHeader/>
+                <ProductList 
+                products = {products}
+                onSelectProduct = {handleSelectProduct}
+                selectedProducts = {selectedProducts}
+                />
+                <OrderLocation 
+                onChangeLocation = {
+                    location => setOrderLocation(location)
+                    }
+                />
+                <OrderSummary 
+                amount = {selectedProducts.length} 
+                totalPrice = {totalPrice}
+                />
+            </div>
+            <Footer/>
+        </>
     );
 }
 
