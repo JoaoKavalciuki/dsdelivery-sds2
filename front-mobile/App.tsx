9c41b5cfdadb12135c6077f5280b871e3684db5c
João Kavalciuki <joaokavalciuki@hotmail.com>
2021-01-16 04:59:40 -0300

    Add order list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -6,6 +6,7 @@ import AppLoading from 'expo-app-loading';
 import Header from './src/header';
 import Home from './src/home'
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -20,8 +21,7 @@ export default function App() {
   return (
     <View style={styles.container}>      
       <StatusBar style="auto" />
-      <Header/>
-      <Home/>
+      <Routes/>
     </View>
   );
 }
